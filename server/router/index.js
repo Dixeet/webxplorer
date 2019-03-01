c0d7056906d5004bf76b1a843f192dc4185f3b49
Robin <robin.lebert@gmail.com>
2019-03-01 18:10:23 +0100

    Test commit

--- server/router/index.js
+++ server/router/index.js
@@ -10,7 +10,7 @@ module.exports = function(nuxt) {
   if (nuxt) {
     router.get('*', ctx => {
       ctx.status = 200;
-      ctx.respond = false; // Bypass Koa's built-in response handling
+      ctx.respond = false; // Bypass Koa's built-in response handling test
       ctx.req.ctx = ctx; // This might be useful later on, e.g. in nuxtServerInit or with nuxt-stash
       nuxt.render(ctx.req, ctx.res);
     });
