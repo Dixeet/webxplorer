af135ae0367158ec22097d97c300839d9c15387b
Robin <robin.lebert@gmail.com>
2019-03-04 11:25:50 +0100

    webxplorer-10 Add notification system #10

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,3 +3,40 @@
     <nuxt />
   </div>
 </template>
+
+<script>
+export default {
+  data: () => ({
+    isMounted: false,
+  }),
+
+  computed: {
+    notification() {
+      return this.$store.state.notification;
+    },
+  },
+
+  watch: {
+    notification: function(notify) {
+      this.notify(notify);
+    },
+    isMounted: function(isMounted) {
+      if (isMounted && this.notification.message) {
+        this.notify(this.notification);
+      }
+    },
+  },
+
+  mounted() {
+    this.isMounted = true;
+  },
+
+  methods: {
+    notify: function(notif) {
+      if (this.isMounted) {
+        this.$toast.open({ message: notif.message, type: notif.type });
+      }
+    },
+  },
+};
+</script>
--- store/index.js
+++ store/index.js
@@ -1,6 +1,19 @@
 export const state = () => ({
   config: {},
+  notification: { message: '', type: '' },
 });
+
+export const mutations = {
+  notify(
+    state,
+    notification = {
+      message: '',
+      type: '',
+    },
+  ) {
+    state.notification = notification;
+  },
+};
 
 export const actions = {
   nuxtServerInit({ state }, { req }) {
