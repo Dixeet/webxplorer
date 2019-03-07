38c1a0cf20f08566bdd72799dbe57081b6d93cf8
Robin <robin.lebert@gmail.com>
2019-03-07 18:56:24 +0100

    webxplorer-13 Add and refactor config api endpoints #13

--- server/router/api/config/index.js
+++ server/router/api/config/index.js
@@ -1,6 +1,8 @@
 const Router = require('koa-router');
+const Dot = require('dot-object');
 const services = require('../../../services');
 const router = new Router();
+const dot = new Dot(':');
 
 function saveConf() {
   return new Promise((resolve, reject) => {
@@ -13,17 +15,37 @@ function saveConf() {
   });
 }
 
-router.post('/', async ctx => {
+const mask = {
+  'server:rootDir': services.config.get('server:rootDir'),
+  'server:apiBaseUrl': services.config.get('server:apiBaseUrl'),
+  'server:auth:enable': services.config.get('server:auth:enable'),
+  'server:auth:jwt-secret': services.config.get('server:auth:jwt-secret'),
+  'server:auth:tokenExpires': services.config.get('server:auth:tokenExpires'),
+};
+
+async function postConfig(ctx) {
   if (ctx.request.body) {
-    const keyValues = services.nconfify(ctx.request.body);
-    for (const keyValue of keyValues) {
-      services.config.set(keyValue[0], keyValue[1]);
+    const dottedObject = dot.dot(ctx.request.body);
+    for (const key in dottedObject) {
+      if (Object.keys(mask).indexOf(key) > -1) {
+        services.config.set(key, dottedObject[key]);
+      } else if (key === 'server:auth:password') {
+        // Todo Handle password
+        console.log('password', dottedObject[key]);
+      }
     }
     await saveConf();
   }
   ctx.body = {
     message: 'Config saved',
   };
-});
+}
+
+function getConfig(ctx) {
+  ctx.body = dot.object(mask);
+}
+
+router.post('/', postConfig);
+router.get('/', getConfig);
 
 module.exports = router.routes();
--- server/services/index.js
+++ server/services/index.js
@@ -23,18 +23,6 @@ function find(options) {
   });
 }
 
-function nconfify(obj, currentKey = '', keyValues = []) {
-  for (const entry of Object.entries(obj)) {
-    const key = currentKey + entry[0];
-    if (typeof entry[1] !== 'object') {
-      keyValues.push([key, entry[1]]);
-    } else {
-      nconfify(entry[1], key + ':', keyValues);
-    }
-  }
-  return keyValues;
-}
-
 async function getDownloadHeaders(options) {
   let headers = [];
   if (options.path) {
@@ -102,5 +90,4 @@ module.exports = {
   decodePath,
   find,
   getDownloadHeaders,
-  nconfify,
 };
