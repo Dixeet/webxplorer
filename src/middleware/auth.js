4c5cb64dc7219c1bc2b152d261431909bc450e24
Robin <robin.lebert@gmail.com>
2019-03-07 12:55:13 +0100

    webxplorer-12 Done auth middleware #12

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,5 +1,29 @@
-export default function({ store, redirect }) {
-  if (store.state.config.authEnable) {
-    redirect('/login');
+import Cookie from 'js-cookie';
+
+export default function({ store, redirect, route, query }) {
+  if (store.state.config.server.auth.enable) {
+    if (process.server && !store.state.isLogged) {
+      if (route.path !== '/login') {
+        redirect('/login', { redirect: 'authenticate' });
+      }
+      if (query.redirect && query.redirect === 'authenticate') {
+        store.dispatch('notify', {
+          message: 'You need to be authenticate',
+          type: 'is-danger',
+        });
+      }
+    }
+    if (process.client && (!Cookie.get('jwt-token') || !store.state.isLogged)) {
+      if (store.state.isLogged) {
+        store.commit('isNotLogged');
+      }
+      if (route.path !== '/login') {
+        store.dispatch('notify', {
+          message: 'You need to be authenticate',
+          type: 'is-danger',
+        });
+        redirect('/login');
+      }
+    }
   }
 }
