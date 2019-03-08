e2de94627ec5b8a2309329446363ca94abf41daa
Robin <robin.lebert@gmail.com>
2019-03-08 14:45:50 +0100

    webxplorer-14 Remove adm-zip for archiver and refactor #14

--- server/router/api/download/zip.js
+++ server/router/api/download/zip.js
@@ -1,55 +1,60 @@
 const fs = require('fs-extra');
-const AdmZip = require('adm-zip');
+const archiver = require('archiver');
 const services = require('../../../services');
-
-function toBuff(zip) {
-  return new Promise((resolve, reject) => {
-    zip.toBuffer(
-      buff => {
-        resolve(buff);
-      },
-      err => {
-        reject(err);
-      },
-    );
-  });
-}
 
 function getZipName(path) {
   const lastPart = path.split('/').splice(-1, 1)[0];
   return `${lastPart.split('.')[0]}.zip`;
 }
 
-module.exports = function() {
-  return async function zipMiddleware(ctx) {
-    const path = services.decodePath(ctx.params.path);
+async function zippify(
+  ctx,
+  entries,
+  options = { name: 'selection.zip', asRoot: true },
+) {
+  const zip = archiver('zip', {
+    zlib: { level: 3 },
+  });
+  ctx.body = zip;
+  const date = new Date();
+  ctx.set('Date', date.toUTCString());
+  ctx.set('Content-Disposition', `attachment; filename="${options.name}"`);
+  for (const entry of entries) {
+    const path = services.decodePath(entry.path);
     const fullPath = services.resolve(
       services.config.get('server:rootDir'),
       path,
     );
     const stat = await fs.stat(fullPath);
-    const zip = new AdmZip();
-    let buff = null;
-    if (stat.isFile()) {
-      zip.addLocalFile(fullPath);
-      buff = await toBuff(zip);
+    let name = '';
+    if (typeof entry.asRoot === 'undefined') {
+      name = options.asRoot ? path.split('/').splice(-1, 1)[0] : path;
+    } else {
+      name = entry.asRoot ? path.split('/').splice(-1, 1)[0] : path;
     }
     if (stat.isDirectory()) {
-      const files = await fs.readdir(fullPath);
-      if (files.length > 0) {
-        zip.addLocalFolder(fullPath);
-        buff = await toBuff(zip);
-      } else {
-        ctx.throw(416, 'Folder is empty');
-      }
+      zip.directory(fullPath, name);
     }
-    const date = new Date();
-    ctx.set('Date', date.toUTCString());
-    ctx.set('Content-Length', buff.length);
-    ctx.set(
-      'Content-Disposition',
-      `attachment; filename="${getZipName(path)}"`,
+    if (stat.isFile()) {
+      zip.file(fullPath, { name: name });
+    }
+  }
+  zip.finalize();
+}
+
+module.exports = function() {
+  return async function zipMiddleware(ctx) {
+    await zippify(
+      ctx,
+      [
+        {
+          path: ctx.params.path,
+        },
+      ],
+      {
+        name: getZipName(services.decodePath(ctx.params.path)),
+        asRoot: true,
+      },
     );
-    ctx.body = buff;
   };
 };
